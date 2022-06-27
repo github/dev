3a76bd0e0d769d7d260cef733c68255c03e251e8
peterliuxb <peterlxb@gmail.com>
2022-06-27 21:26:40 +0800

    Add test

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Solution {
+    private String name;
+    private int age;
+
+    public Solution(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main() {
+        System.out.println("test");
+    }
+}
