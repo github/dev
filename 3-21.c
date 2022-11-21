4671077533b44ef007937794e7dd13f7a0578561
cocacocca <56760449+cocacocca@users.noreply.github.com>
2022-11-21 02:43:32 +0000

    new file:   3-21 	new file:   3-21.c 	deleted:    9*9.c 	renamed:    9*9 -> 9_9 	deleted:    print week.py 	new file:   print_week.py

---
+++ 3-21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int m,n,r;
+    printf("请输入两个正整数：");
+	scanf("%d,%d",&m,&n);
+	do
+    {
+        r=m%n;
+        m=n;
+        n=r;
+    }while(n!=0);
+    printf("以上两数的最大公因子：%d\n",m);
+
+}
