d65472058ed001c0a743dcec61f5a8de15bace56
Ticka971 <54371977+Ticka971@users.noreply.github.com>
2022-12-14 14:36:27 +0100

    ok

--- script.js
+++ script.js
@@ -82,5 +82,12 @@ window.onload = () => {
 
 
 
+ let icons=document.querySelectorAll(".ico");let length=icons.length;icons.forEach((item,index)=>{item.addEventListener("mouseover",(e)=>{focus(e.target,index);});item.addEventListener("mouseleave",(e)=>{icons.forEach((item)=>{item.style.transform="scale(1)  translateY(0px)";});});});const focus=(elem,index)=>{let previous=index-1;let previous1=index-2;let next=index+1;let next2=index+2;if(previous==-1){console.log("first element");elem.style.transform="scale(2) translateY(-10px)";}else if(next==icons.length){elem.style.transform="scale(2)  translateY(-10px)";console.log("last element");}else{elem.style.transform="scale(2)  translateY(-10px)";icons[previous].style.transform="scale(1.2) translateY(-6px)";icons[previous1].style.transform="scale(1.1)";icons[next].style.transform="scale(1.2) translateY(-6px)";icons[next2].style.transform="scale(1.1)";}};
+
+
+
+ function highlight(selector){$(selector).mouseenter(function(){var classLink=$(this).attr('class').split(' ')[1];$("."+classLink).addClass('over');$(".section-"+classLink).stop().fadeIn();}).mouseleave(function(){var classLink=$(this).attr('class').split(' ')[1];$("."+classLink).removeClass('over');$(".section-"+classLink).stop().fadeOut();});};;
+
+
 } // fin window.onload
 
