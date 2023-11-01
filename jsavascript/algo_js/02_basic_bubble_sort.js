35e261cd3c270a9944a9df54f5c17990abc59b9e
Jatin Saini <jatinrajbagri2001@gmail.com>
2023-11-01 17:57:43 +0530

    added more algo in js section

--- jsavascript/algo_js/02_basic_bubble_sort.js
+++ jsavascript/algo_js/02_basic_bubble_sort.js
@@ -4,9 +4,10 @@ let swaped = 1
 while (swaped != 0)
     {
         swaped = 0;
-        for (let i = 0 ; i< a.length;i++){
-            let indexOfNum = i+1
-            let currIndex = i
+        // for loop is till -1 because we don't need to compare last element with and out bound element
+        for (let i = 0 ; i< a.length-1;i++){
+            // let indexOfNum = i+1
+            // let currIndex = i
             if (a[i+1] < a[i]){
                 // console.log('swapped',a[indexOfNum], a[currIndex]);
                 [a[i+1], a[i]] = [a[i],a[i+1]];
@@ -16,3 +17,23 @@ while (swaped != 0)
         
     }
 console.log(a)
+
+
+// Improved bubble sort => time complexity = O^n2
+
+function bubbleSort(arr) {
+    let swapped
+    do {
+      swapped = false
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            [arr[i+1], arr[i]] = [arr[i],arr[i+1]];
+          swapped = true
+        }
+      }
+    } while (swapped)
+  }
+  
+  const arr = [8, 20, -2, 4, -6]
+  bubbleSort(arr)
+  console.log(arr) // [-6, -2, 4, 8, 20]
